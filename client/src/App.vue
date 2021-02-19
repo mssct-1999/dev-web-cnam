142c1806bbdf8450e6745321d5481502ea33d662
Hippolyte Massicot <hippolyte.massicot@gmail.com>
2021-02-20 00:28:15 +0100

    feat(HomeComponent): début front homepage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand">
+      <div id="brand-container">
+        <router-link to="/" class="navbar-brand">
+          <img alt="Logo Association Sophrofraise" src="./assets/img/logo.png">
+          <span id="brand-text">Association Sophrofraise</span>
+        </router-link>
+      </div>
+      <div id="menu-container">
+        <div class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/presentation" class="nav-link">Présentation</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link">Contact</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link"><i class="fas fa-user"></i></router-link>
+          </li>
+        </div>
+      </div>
+    </nav>
+
+    <div>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
+<!-- CSS appliqué à tous les composants  -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color:#fff0bc !important;
+  }
+</style>
+
+<!-- CSS uniquement pour le component -->
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+JP:wght@100&display=swap');
+  .navbar.navbar-expand {
+    /**box-shadow: 0 2px 2px -2px rgba(0,0,0,.2);**/
+    display:flex;
+    justify-content:space-between;
+  }
+
+  .nav-link {
+    /**color:#D3C8B7 !important;**/
+    color:black;
+    font-size: 17px !important;
+    font-family:'Noto Sans JP',sans-serif;
+    font-weight:bolder;
+  }
+
+  .navbar-brand {
+    display:flex;
+    flex-direction:column;
+    font-family: 'Lobster',cursive;
+    font-size:13px;
+    justify-content:center;
+  }
+
+  .navbar-brand img {
+    width:100px;
+    margin-left:7px;
+  }
+
+  #brand-text {
+    position:relative;
+    bottom:11px;
+    color:black;
+    font-weight:bolder;
+  }
 </style>
---
+++ client/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <div id="wallpaper-container">
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    #wallpaper {
+        max-width:100%;
+        height:50%;
+    }
+
+    #wallpaper-container {
+        width:100%;
+        height:1300px;
+    }
+</style>
