c494d6e3a01a66486fa7504df9ddb52ed45f202e
Hippolyte Massicot <hippolyte.massicot@gmail.com>
2021-02-21 19:45:01 +0100

    feat(LoadingComponent): front loading component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,68 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand">
-      <div id="brand-container">
-        <router-link to="/" class="navbar-brand">
-          <img alt="Logo Association Sophrofraise" src="./assets/img/logo.png">
-          <span id="brand-text">Association Sophrofraise</span>
-        </router-link>
-      </div>
-      <div id="menu-container">
-        <div class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link to="/presentation" class="nav-link">Présentation</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/contact" class="nav-link">Contact</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/login" class="nav-link"><i class="fas fa-user"></i></router-link>
-          </li>
+    <div v-if="isLoading"> 
+      <LoadingComponent v-if="isLoading"></LoadingComponent>
+    </div>
+    <div v-else>
+      <nav class="navbar navbar-expand">
+        <div id="brand-container">
+          <router-link to="/" class="navbar-brand">
+            <img alt="Logo Association Sophrofraise" src="./assets/img/logo.png">
+            <span id="brand-text">Association Sophrofraise'tralala</span>
+          </router-link>
         </div>
+        <div id="menu-container">
+          <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link to="/presentation" class="nav-link"><img src="./assets/icons/strawberry.svg" height="20px" width="20px" alt="Icône de fraise">Présentation</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/contact" class="nav-link">Contact</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link"><i class="fas fa-user"></i>Connexion</router-link>
+            </li>
+          </div>
+        </div>
+      </nav>
+      <!-- content of the page -->
+      <div>
+        <router-view />
       </div>
-    </nav>
-
-    <div>
-      <router-view />
     </div>
   </div>
 </template>
 
 <script>
+import LoadingComponent from './components/LoadingComponent.vue';
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    LoadingComponent
+  },
+  data: function() {
+    return {
+      isLoading:true
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false
+    },10000)
+  }
 };
 </script>
 
 <!-- CSS appliqué à tous les composants  -->
 <style>
   body {
-    background-color:#fff0bc !important;
+    background-color:#BAD5CA !important;
   }
 </style>
 
 <!-- CSS uniquement pour le component -->
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+JP:wght@100&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Pontano+Sans&family=Lobster&family=Noto+Sans+JP:wght@100&display=swap');
   .navbar.navbar-expand {
     /**box-shadow: 0 2px 2px -2px rgba(0,0,0,.2);**/
     display:flex;
@@ -54,21 +73,26 @@ export default {
     /**color:#D3C8B7 !important;**/
     color:black;
     font-size: 17px !important;
-    font-family:'Noto Sans JP',sans-serif;
+    font-family:'Pontano Sans',sans-serif;
     font-weight:bolder;
   }
 
   .navbar-brand {
     display:flex;
     flex-direction:column;
-    font-family: 'Lobster',cursive;
-    font-size:13px;
+    font-family: 'Pontano Sans',sans-serif;
+    font-size:10px;
     justify-content:center;
+    padding-left: 100px;
+  }
+
+  #menu-container {
+    padding-right: 100px;
   }
 
   .navbar-brand img {
     width:100px;
-    margin-left:7px;
+    margin-left: 8px;
   }
 
   #brand-text {
---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,123 @@
+<template>
+    <div id="main">
+        <div id="loading">
+            <div id="loading-content">
+                <span id="brand-text">Sophrofraise'tralala</span>
+                <img id="image-loader" src="../assets/img/strawberry.png" />
+            </div>
+        </div>
+        <div class="div-animation" id="div-text-visible">association</div>
+        <div class="div-animation" id="div-text-hidden">
+            <span>pour répandre le mieux être et le mieux manger</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'LoadingComponent'
+}
+</script>
+
+<style scoped>
+    #main {
+        height:100vh;
+        display:flex;
+        justify-content: center;
+        align-items:center;
+    }
+
+    #loading {
+        background-color:#BAD5CA;
+    }
+
+    #loading-content {
+        color:black;
+    }
+
+    #brand-text {
+        font-size:40px;
+        font-family:'Pontano Sans',sans-serif;
+        font-weight:bolder;
+    }
+
+    #image-loader {
+        margin-top:-20px;
+        margin-left:-20px;
+        -webkit-animation: tp-rotateplane 2.5s infinite ease-in-out;
+        animation: tp-rotateplane 2.5s infinite ease-in-out;
+        border-radius: 3px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        -webkit-animation-fill-mode: none !important;
+        animation-fill-mode: none !important;
+        width: 90px;
+        margin-left:20px;
+    }
+
+    #div-text-hidden span {
+        margin-left: -355px;
+        animation: slidein 15s infinite;
+    }
+
+    #div-text-hidden {
+        width: 0px;
+        animation: reveal 15s infinite;
+        font-weight:bolder;
+    }
+
+    #div-text-visible {
+    animation: showup 15s infinite;
+    font-weight:bolder;
+}
+
+    .div-animation {
+        display: inline-block;
+        overflow: hidden;
+        white-space: nowrap;
+        font-family:'Pontano Sans',sans-serif;
+        font-size:20px;
+    }
+
+    @keyframes showup {
+        0% {opacity:0;}
+        20% {opacity:1;}
+        80% {opacity:1;}
+        100% {opacity:0;}
+    }
+
+    @keyframes slidein {
+        0% { margin-left:-800px; }
+        20% { margin-left:-800px; }
+        35% { margin-left:5px; }
+        100% { margin-left:5px; }
+    }
+
+    @keyframes reveal {
+        0% {opacity:0;width:0px;}
+        20% {opacity:1;width:0px;}
+        30% {width:500px;}
+        80% {opacity:1;}
+        100% {opacity:0;width:500px;}
+    }
+
+
+    @keyframes tp-rotateplane {
+        0% { 
+            transform: perspective(120px) rotateX(0deg) rotateY(0deg);
+            -webkit-transform: perspective(120px) rotateX(0deg) rotateY(0deg) 
+        } 50% { 
+            transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg);
+            -webkit-transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg) 
+        } 100% { 
+            transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
+            -webkit-transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
+        }
+    }
+
+    @-webkit-keyframes tp-rotateplane {
+    0% { -webkit-transform: perspective(120px) }
+    50% { -webkit-transform: perspective(120px) rotateY(180deg) }
+    100% { -webkit-transform: perspective(120px) rotateY(180deg)  rotateX(180deg) }
+    }
+</style>
