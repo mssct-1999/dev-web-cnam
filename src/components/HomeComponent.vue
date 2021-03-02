618768c13eebebdd8ba7bc5078359b1cd74cc4c1
Hippolyte Massicot <hippolyte.massicot@gmail.com>
2021-03-02 17:48:15 +0100

    feat(Homepage): avancée sur le front, début page d'accueil

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,18 +1,39 @@
 <template>
-    <section>
-        <div id="wallpaper-container">
+    <div id="main">
+        <div id="second-navbar">
+            <PaintComponentImage class="paint-link" width="130" mgTopPx="0" title="Présentation"></PaintComponentImage>
+            <PaintComponentImage class="paint-link" width="130" mgTopPx="100" title="Qui suis-je ?"></PaintComponentImage>
+            <PaintComponentImage class="paint-link" width="150" mgTopPx="0" title="Où me trouvez ?"></PaintComponentImage>
+            <PaintComponentImage class="paint-link" width="130" mgTopPx="50" title="Contact"></PaintComponentImage>
         </div>
-    </section>
+        <PresentationComponent></PresentationComponent>
+    </div>
 </template>
 
+<script>
+import PaintComponentImage from "./PaintComponentImage" 
+import PresentationComponent from "./PresentationComponent"
+
+export default {
+  components: { PaintComponentImage, PresentationComponent},   
+}
+</script>
+
 <style scoped>
-    #wallpaper {
-        max-width:100%;
-        height:50%;
+    #second-navbar {
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        padding:50px;
+        margin-bottom:70px;
     }
 
-    #wallpaper-container {
-        width:100%;
-        height:1300px;
+    #second-navbar * {
+        margin-right:100px;
+    }
+
+    #second-navbar {
+        display:flex;
+        justify-content: center;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router"
+import vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+Vue.use(vuetify)
 
 new Vue({
+  vuetify,
   router,
   render: h => h(App),
 }).$mount('#app')
